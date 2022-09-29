0a1f587db0d37216b3cd635511685bdfa4d44801
Hemant Chaubey <89316018+imhemantchaubey@users.noreply.github.com>
2022-09-29 21:21:23 +0530

    Updates

---
+++ precache-manifest.1d4739f2dfdcbc0ca52a48499e58c5d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fce5402b3e0ea79641f6",
+    "url": "/textutils/static/css/main.19c6197c.chunk.css"
+  },
+  {
+    "revision": "fce5402b3e0ea79641f6",
+    "url": "/textutils/static/js/main.0a149e78.chunk.js"
+  },
+  {
+    "revision": "5290dc96bc51c2b1fbd3",
+    "url": "/textutils/static/js/runtime~main.0e78248a.js"
+  },
+  {
+    "revision": "dcb29218c3bef81316b8",
+    "url": "/textutils/static/js/2.49ab7afa.chunk.js"
+  },
+  {
+    "revision": "db569b567d12d48e4e61",
+    "url": "/textutils/static/js/3.d51c813c.chunk.js"
+  },
+  {
+    "revision": "b748b3d7d11af4a2fe37b9a8b530e3dc",
+    "url": "/textutils/index.html"
+  }
+];
